669fa79ffd4fe6eb4b892ad92d337f54deab62c7
Dhruv <samantdhruv@gmail.com>
2021-09-16 10:16:44 +0530

    date to string feat

--- index.js
+++ index.js
@@ -11,7 +11,34 @@ function isPalindrome(str) {
     var reverse = reversedStr(str);
     if ( str === reverse) {
         return true;
-    } else 
+    } else {
     return false;
+    }
 }
 
+function convertDateToStr(date) {
+    var dateStr = { day: '', month: '', year: '' };
+    if (date.day < 10) {
+        dateStr.day = '0' + date.day;
+    } else {
+        dateStr.day = date.day.toString();
+    }
+
+    if (date.month < 10) {
+        dateStr.month = '0' + date.month;
+    } else {
+        dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr;
+}
+
+var date = {
+    day: 15,
+    month: 11,
+    year: 2020
+}
+
+console.log(convertDateToStr(date));
